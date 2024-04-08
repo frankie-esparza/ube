5e6d4ad49623a0cf654199e05ae55e7f99a351f1
Frankie Willcox Esparza <frankie@madisons-mbp-2.lan>
2024-04-08 13:44:14 -0700

    create order form workingng, added helpers for both create/update forms, removed legacy sql-alchemy syntax

--- app/routes/session.py
+++ app/routes/session.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, url_for, render_template
 from app.forms.Login import LoginForm
-from app.models import Employee
+from app.models import db, Employee
 from flask_login import current_user, login_user, logout_user
 
 bp = Blueprint("session", __name__, url_prefix="/session")
@@ -11,21 +11,26 @@ def login():
         return redirect(url_for("orders.index"))
     
     form = LoginForm()
-
     if form.validate_on_submit():
-        empl_number = form.employee_number.data
-        employee = Employee.query.filter(Employee.employee_number == empl_number).first()
-        
-        if not employee or not employee.check_password(form.password.data):
-            return redirect(url_for(".login"))
-        
-        login_user(employee)
-        return redirect(url_for("orders.index"))
-    
+        handleLoginFormSubmit(form)
+
     return render_template("forms/form.html", form=form, path='session.login')
+
 
 @bp.route("/logout", methods=["POST"])
 def logout():
     logout_user()
     return redirect(url_for("orders.index"))
 
+# -----------
+# HELPERS
+# -----------
+def handleLoginFormSubmit(form):
+    employee = db.first_or_404(
+        db.select(Employee).where(Employee.employee_number == form.employee_number.data))
+
+    if not employee or not employee.check_password(form.password.data):
+        return redirect(url_for(".login"))
+    
+    login_user(employee)
+    return redirect(url_for("orders.index"))
